c5555c81bf3d4a8ef950e1e5c0a3797f54e8bd71
JC <bonassin@gmail.com>
2020-12-21 17:08:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
-def reverse_each_word(string)
-  string.each do |string|
-    puts "#{string.reverse}"
-  end
+ef reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
